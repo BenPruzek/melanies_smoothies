684a54c3d7ab5290a2648ff52873b4b9e4855e1e
BenPruzek <benjamin.pruzek@gmail.com>
2026-06-23 09:51:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,9 @@ session = cnx.session()
 
 # 2. REMOVED the old get_active_session() line that was causing the crash
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.datafram(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
