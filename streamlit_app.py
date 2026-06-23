4da8787b4aed6d33361ffde47fcc21c1f9583b04
BenPruzek <benjamin.pruzek@gmail.com>
2026-06-23 10:06:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,11 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd #  Added this import so pandas commands work
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
-st.write(
-  """Choose the fruits you want in your custom Smoothie!
-  """
-)
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input('Name on Smoothie: ')
 st.write('The name on our Smoothie will be:', name_on_order)
@@ -17,15 +15,10 @@ st.write('The name on our Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# 2. REMOVED the old get_active_session() line that was causing the crash
-
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-# st.stop()
 
-pd_df = my_dateframe.to_pandas()
-# st.dataframe(pd_df)
-# st.stop()
+# 2. Fixed spelling typo from my_dateframe to my_dataframe
+pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -39,12 +32,14 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on=pd.df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen, ' is ' , search_on, ',')
-
+        # 3. Fixed the dot typo to an underscore (pd_df)
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
       
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        
+        # 4. Tweak the API URL to use your brand new search_on variable instead of fruit_chosen!
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
   
@@ -54,8 +49,5 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
-        # 3. Use the correct session object to push the data to Snowflake
         session.sql(my_insert_stmt).collect()
-        
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
-
